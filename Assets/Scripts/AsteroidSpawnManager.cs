0b52a5992acfbb9fab1380957a113c880bf98a57
Edgar Espino <edgar.espino@loop.colum.edu>
2020-10-18 20:42:48 -0500

    Added asteroid prefabs, colliders, and basic gameplay interaction scripts

---
+++ Assets/Scripts/AsteroidSpawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawnManager : MonoBehaviour
+{
+    private float spawnPosX = 125f;
+    private float spawnRangeY = 85f;
+    private float spawnPosZ = 0.0f;
+    public float spawnInterval = 1.25f;
+    public float startDelay;
+
+    // Spawn manager array for enemy prefabs
+    public GameObject[] enemyPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Method to call a function at a certain time
+        InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    // Custom functions to spawn random enemies
+    void SpawnRandomEnemy()
+    {
+        // Randomly generate enemies
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        Vector3 spawnPos = new Vector3(spawnPosX, Random.Range(-spawnRangeY, spawnRangeY), spawnPosZ);
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+    }
+}
+
---
+++ Assets/Scripts/SpinEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinEffect : MonoBehaviour
+{
+    [SerializeField] float spinSpeed;
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.angularVelocity = Random.insideUnitSphere * spinSpeed;
+    }
+}
