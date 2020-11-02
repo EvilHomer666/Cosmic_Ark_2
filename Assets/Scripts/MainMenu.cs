1e4cd2f5c391701f961612ceda89e1fb6b23d795
Edgar Espino <edgar.espino@loop.colum.edu>
2020-11-02 12:09:42 -0600

    Added main menu fonts and basic menu layout

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject credits;
+
+    // Custom methods to show credits, main menu, start & quit game
+    public void ShowCredits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
