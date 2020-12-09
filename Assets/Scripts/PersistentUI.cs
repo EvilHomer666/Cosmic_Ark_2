8de7e6b7a92362ee10f3e69455de4f84c29df3f0
Edgar Espino <edgar.espino@loop.colum.edu>
2020-12-09 16:44:06 -0600

    Attempt to better handle singleton for shields

---
+++ Assets/Scripts/PersistentUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentUI : MonoBehaviour
+{
+    // Singleton used to carry persistent UI across scenes
+    public static PersistentUI Instance { get; private set; }
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
