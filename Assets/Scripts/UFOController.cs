9ab95b63821654e525340e356a51c1598ce92e83
Edgar Espino <edgar.espino@loop.colum.edu>
2020-12-10 16:01:56 -0600

    Added animal prefabs and outsource models and UFO controller

---
+++ Assets/Scripts/UFOController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOController : MonoBehaviour
+{
+    [SerializeField] float playerSpeed;
+    private float xRange = 85f;
+    private float yRangeUp = 5f;
+    private float yRangeDown = 60f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    void Update()
+    {
+        // Check for horizontal & vertical player movement boundary
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y < -yRangeDown)
+        {
+            transform.position = new Vector3(transform.position.x, -yRangeDown, transform.position.z);
+        }
+        if (transform.position.y > yRangeUp)
+        {
+            transform.position = new Vector3(transform.position.x, yRangeUp, transform.position.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Player input movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerSpeed, Space.World);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * playerSpeed, Space.World);
+    }
+}
