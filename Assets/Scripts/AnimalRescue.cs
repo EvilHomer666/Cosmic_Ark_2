2de0880ed96198939cfb9eebea8f0d39112db17b
Edgar Espino <edgar.espino@loop.colum.edu>
2020-12-13 18:59:48 -0600

    Added basic animal rescue script

---
+++ Assets/Scripts/AnimalRescue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalRescue : MonoBehaviour
+{
+    private DetectPlayerCollisions playerRescueBonus;
+    private int recuePoint = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Detect when animal reaches the ship
+        if (other.gameObject.tag == "Creature")
+        {
+            playerRescueBonus.playerCurentHitpoints += recuePoint;
+            Destroy(other.gameObject);
+        }
+    }
+}
