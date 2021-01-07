6420a05506ff800ba911931b512e67205cbe2f54
Edgar Espino <edgar.espino@loop.colum.edu>
2021-01-07 02:48:19 -0600

    Revert "Revert "Adjusted tractor beam light alpha and debugged singleton""
    This reverts commit 43dfc4446d222ddde8e1993ed67f509500148443.

--- Assets/Scripts/Timers.cs
+++ Assets/Scripts/Timers.cs
@@ -8,6 +8,8 @@ public class Timers : MonoBehaviour
 {
     [SerializeField] Text timerText;
     [SerializeField] float roundTime; // Start level time at 60 sec. This number will change depending on game progression.
+    private ShieldManager shieldData;
+    private ScoreManager scoreData;
     private LevelTransition sceneManager;
     private float timerLimit = 0.005f;
     
@@ -16,7 +18,8 @@ public class Timers : MonoBehaviour
     void Start()
     {
         sceneManager = FindObjectOfType<LevelTransition>();
-
+        shieldData = FindObjectOfType<ShieldManager>();
+        scoreData = FindObjectOfType<ScoreManager>();
         // TO DO add condition to check score and adjust round time based on game progress.
     }
 
@@ -35,6 +38,8 @@ public class Timers : MonoBehaviour
 
         if (roundTime < timerLimit)
         {
+            shieldData.SavePlayerData();
+            scoreData.SavePlayerData();
             sceneManager.MoveToNextLevel();
         }
     }
